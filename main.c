4d13f2682b8dddd8a027bcc796c39d78672fc81d
josephgabriel <josgabrielsoares825@gmail.com>
2024-11-18 19:52:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-    
-    return 0;
+    void PreencherHistograma (char*h[], int V, int C)
+
+{
+    for(int i = 0; i < V; i++)
+    {
+        h[C][i] = '*';
+    }
+}
+
+void imprimir_histograma(char *h[], int V[], int C)
+
+{
+	
+for(int i = 0; i < C; i++)
+
+{
+	
+printf("%d", V[i]);
+
+for (int j = 0; j < V[i]; j++)
+
+{
+	
+printf("%c", h[i][j]);
+
+  }
+  
+printf("\n");
+
+  }
+  
+}
+
+int main()
+{
+
+int C;
+scanf("%d", &C);
+
+int V [C];
+char *histograma[C];
+
+for(int i = 0; i < C; i++)
+
+{
+	
+scanf("%d", &V[i]);
+if(V[i] == 0)
+
+{
+	
+histograma[i] = NULL;
+
+}
+
+
+else 
+
+{
+	
+histograma[i] = (char *)malloc(V[i]*sizeof(char));
+PreencherHistograma(histograma, V[i], i);
+
+}
+
+}
+
+imprimir_histograma(histograma, V, C);
+return 0;
+
 }
