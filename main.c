656f879ba54dcbde8a85b3321fe28af3931465f3
josephgabriel <josgabrielsoares825@gmail.com>
2024-11-19 00:00:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,70 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-    void PreencherHistograma (char*h[], int V, int C)
-
-{
-    for(int i = 0; i < V; i++)
-    {
+void PreencherHistograma(char *h[], int V, int C) {
+    for (int i = 0; i < V; i++) {
         h[C][i] = '*';
     }
 }
 
-void imprimir_histograma(char *h[], int V[], int C)
-
-{
-	
-for(int i = 0; i < C; i++)
-
-{
-	
-printf("%d", V[i]);
-
-for (int j = 0; j < V[i]; j++)
-
-{
-	
-printf("%c", h[i][j]);
-
-  }
-  
-printf("\n");
-
-  }
-  
+void imprimir_histograma(char *h[], int V[], int C) {
+    for (int i = 0; i < C; i++) {
+        printf("%d", V[i]);
+        for (int j = 0; j < V[i]; j++) {
+            printf("%c", h[i][j]);
+        }
+        printf("\n");
+        printf("\n");
+    }
 }
 
-int main()
-{
+int main() {
+    int C;
+    scanf("%d", &C);
 
-int C;
-scanf("%d", &C);
+    int V[C];
+    char *histograma[C];
 
-int V [C];
-char *histograma[C];
+    for (int i = 0; i < C; i++) {
+        scanf("%d", &V[i]);
+        if (V[i] == 0) {
+            histograma[i] = NULL;
+        } else {
+            histograma[i] = (char *)malloc(V[i] * sizeof(char));
+            PreencherHistograma(histograma, V[i], i);
+        }
+    }
 
-for(int i = 0; i < C; i++)
-{
-scanf("%d", &V[i]);
-if(V[i] == 0)
-{
-histograma[i] = NULL;
-}else {
-histograma[i] = (char *)malloc(V[i]*sizeof(char));
-PreencherHistograma(histograma, V[i], i);
-}
+    imprimir_histograma(histograma, V, C);
 
-}
+    for (int i = 0; i < C; i++) {
+        if (histograma[i] != NULL) {
+            free(histograma[i]);
+        }
+    }
 
-imprimir_histograma(histograma, V, C);
-
-for(int i = 0; i < C; i++) {
-	if(histograma[i] != NULL) {
-		free(histograma[i]);
-	}
-}
-
-return 0;
-
+    return 0;
 }
