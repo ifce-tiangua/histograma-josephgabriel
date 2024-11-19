b025a622cdc5a3c54d27c930930043c3e55dfdcd
josephgabriel <josgabrielsoares825@gmail.com>
2024-11-19 00:22:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,13 +9,14 @@ void PreencherHistograma(char *h[], int V, int C) {
 
 void imprimir_histograma(char *h[], int V[], int C) {
     for (int i = 0; i < C; i++) {
-        printf("%d ", V[i]);
+        printf("%d", V[i]);
         for (int j = 0; j < V[i]; j++) {
             printf("%c", h[i][j]);
         }
-        
-        printf("\\n");
-        
+        if (i < C - 1) {
+            printf("\n");
+        }
+        printf("\n");
     }
 }
 
@@ -40,9 +41,4 @@ int main() {
 
     for (int i = 0; i < C; i++) {
         if (histograma[i] != NULL) {
-            free(histograma[i]);
-        }
-    }
-
-    return 0;
-}
+            free(h
